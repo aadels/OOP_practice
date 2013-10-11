4b99511962d84f34ee968efb937b85cd96889091
ann adelsberger <adelsbergerann@gmail.com>
2013-10-11 17:22:09 -0400

    fixed syntax error

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 		$stephan->set_name("Stephan the Great");
 		$jimmy->set_name("Jiminy Cricket");
 
-		echo "Stephan's Full Name: " . $stephan->get_name():
+		echo "Stephan's Full Name: " . $stephan->get_name();
 		echo "\n";
 
 		echo "Jimmy's Full Name: ". $jimmy->get_name();
