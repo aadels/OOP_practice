71590ee66129dfb2c9514c8960886d5ec71f2961
ann adelsberger <adelsbergerann@gmail.com>
2013-10-11 22:42:28 -0400

    added public property

--- class_lib.php
+++ class_lib.php
@@ -1,10 +1,10 @@
 <?php
 
 class MyClass{
-	//class properties and methods go here.
+	public $prop1 = "I'm a class property!"; 
 }
 $obj = new MyClass;
 
-var_dump ($obj);
+echo $obj->prop1;  
 
 ?>
